f9dce36e36e908449cebbb858056b92c172e1b3d
Arsene Tochemey Gandote <tochemey@hotmail.com>
2020-08-11 20:49:15 +0000

    issue #64: upgrade to lagom-pb 0.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   // Package versions
   object Versions {
     val Scala213 = "2.13.1"
-    val LagompVersion = "0.6-alpha.1"
+    val LagompVersion = "0.6.0"
     val AkkaVersion: String = "2.6.8"
     val KanelaAgentVersion = "1.0.5"
     val SilencerVersion = "1.6.0"
@@ -17,7 +17,7 @@ object Dependencies {
     val LagompbReadSide = "io.superflat" %% "lagompb-readside" % Versions.LagompVersion
 
     val KanelaAgent = "io.kamon" % "kanela-agent" % Versions.KanelaAgentVersion
-    val KamonAkkaGrpc = "com.github.nezasa" %% "kamon-akka-grpc" % Versions.KamonAkkaGrpcVersion intransitive ()
+    val KamonAkkaGrpc = ("com.github.nezasa" %% "kamon-akka-grpc" % Versions.KamonAkkaGrpcVersion).intransitive()
   }
 
   object Runtime {
