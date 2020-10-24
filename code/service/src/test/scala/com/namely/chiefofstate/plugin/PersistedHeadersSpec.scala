ab3489bbf385e46a9d7fedaf798a840bf4cef423
Antonio Yuen <antonio.yuen@namely.com>
2020-10-24 19:09:36 -0400

    enriched spec

--- code/service/src/test/scala/com/namely/chiefofstate/plugin/PersistedHeadersSpec.scala
+++ code/service/src/test/scala/com/namely/chiefofstate/plugin/PersistedHeadersSpec.scala
@@ -11,38 +11,44 @@ class PersistedHeadersSpec extends TestSpec {
   "Persistedheaders" should {
     val foo: String = "foo"
     val fooByteString: ByteString = ByteString.apply(foo)
-    val stringValue: StringValue = StringValue.apply(foo)
-    val stringEntry: StringEntry = StringEntry.apply(foo)
-    val fooHeader: Header = Header.apply(foo, stringValue)
+    val fooStringValue: StringValue = StringValue.apply(foo)
+    val fooStringEntry: StringEntry = StringEntry.apply(foo)
+    val fooHeader: Header = Header.apply(foo, fooStringValue)
 
     val bar: String = "bar"
     val barByteString: ByteString = ByteString.apply(bar)
-    val bytesValue: BytesValue = BytesValue.apply(com.google.protobuf.ByteString.copyFrom(bar.getBytes()))
-    val bytesEntry: BytesEntry = BytesEntry.apply(barByteString)
-    val barHeader: Header = Header.apply(bar, bytesValue)
+    val barBytesValue: BytesValue = BytesValue.apply(com.google.protobuf.ByteString.copyFrom(bar.getBytes()))
+    val barBytesEntry: BytesEntry = BytesEntry.apply(barByteString)
+    val barHeader: Header = Header.apply(bar, barBytesValue)
 
+    val baz: String = "baz"
+    val bazStringValue: StringValue = StringValue.apply(baz)
+    val bazHeader: Header = Header.apply(baz, bazStringValue)
+    
     val metadata: Metadata = Mockito.mock(classOf[Metadata])
     Mockito.when(metadata.getText(foo)).thenReturn(Some(foo))
     Mockito.when(metadata.getBinary(foo)).thenReturn(Some(fooByteString))
     Mockito.when(metadata.getText(bar)).thenReturn(Some(bar))
     Mockito.when(metadata.getBinary(bar)).thenReturn(Some(barByteString))
     Mockito.when(metadata.asMap).thenReturn(Map(
-      foo -> List(stringEntry),
-      bar -> List(bytesEntry)
+      foo -> List(fooStringEntry),
+      bar -> List(barBytesEntry)
     ))
     Mockito.when(metadata.asList).thenReturn(List(
-      (foo, stringEntry),
-      (bar, bytesEntry)
+      (foo, fooStringEntry),
+      (bar, barBytesEntry)
     ))
 
 
     "return the a string and byte header" in {
-      val actual = PersistHeaders
+      // TODO: Have the configs only return foo and bar
+
+      val actual: Headers = PersistHeaders
         .makeMeta(metadata)
         .get
         .unpack[com.namely.protobuf.chiefofstate.plugins.persistedheaders.v1.headers.Headers]
 
-      val expected: Headers = Headers.apply(Vector(fooHeader, barHeader))
+      val expected: Headers = Headers.apply(Vector(fooHeader, barHeader, bazHeader))
 
       actual should be (expected)
     }
