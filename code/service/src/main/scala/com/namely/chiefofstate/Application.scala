bc7b02987f1b4f4db6461c485417d693d4a10827
Arsene Tochemey Gandote <tochemey@hotmail.com>
2020-10-30 22:11:12 +0000

    BRK: fix omission :ambulance:

--- code/service/src/main/scala/com/namely/chiefofstate/Application.scala
+++ code/service/src/main/scala/com/namely/chiefofstate/Application.scala
@@ -1,129 +1,134 @@
 package com.namely.chiefofstate
 
-import akka.actor.CoordinatedShutdown
-import akka.dispatch.MessageDispatcher
-import akka.event.LoggingAdapter
-import akka.grpc.GrpcClientSettings
-import com.lightbend.lagom.scaladsl.akka.discovery.AkkaDiscoveryComponents
-import com.lightbend.lagom.scaladsl.api.Descriptor
-import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
-import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext, LagomApplicationLoader, LagomServer}
-import com.namely.chiefofstate.config.{EncryptionSetting, HandlerSetting, ReadSideSetting, SendCommandSettings}
-import com.namely.chiefofstate.grpc.client.{ReadSideHandlerServiceClient, WriteSideHandlerServiceClient}
-import com.namely.chiefofstate.plugin.{PluginManager, PluginBase}
-import com.namely.chiefofstate.readside.ReadProcessor
-import io.superflat.lagompb.{BaseApplication, CommandHandler, EventHandler}
-import io.superflat.lagompb.encryption.ProtoEncryption
-import kamon.Kamon
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.ActorSystem
+import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, Entity}
+import akka.management.cluster.bootstrap.ClusterBootstrap
+import akka.management.scaladsl.AkkaManagement
+import akka.persistence.typed.PersistenceId
+import akka.util.Timeout
+import com.namely.chiefofstate.config.{CosConfig, ReadSideConfigFactory}
+import com.namely.protobuf.chiefofstate.v1.readside.ReadSideHandlerServiceGrpc.ReadSideHandlerServiceBlockingStub
+import com.namely.protobuf.chiefofstate.v1.service.ChiefOfStateServiceGrpc.ChiefOfStateService
+import com.namely.protobuf.chiefofstate.v1.writeside.WriteSideHandlerServiceGrpc.WriteSideHandlerServiceBlockingStub
+import com.typesafe.config.{Config, ConfigFactory}
+import io.grpc.{ManagedChannel, Server, ServerInterceptors}
+import io.grpc.netty.{NettyChannelBuilder, NettyServerBuilder}
 import org.slf4j.{Logger, LoggerFactory}
 
-/**
- * ChiefOfState application
- *
- * @param context the application context
- */
-abstract class Application(context: LagomApplicationContext) extends BaseApplication(context) {
+import scala.concurrent.ExecutionContext
 
-  lazy val applog: Logger = LoggerFactory.getLogger(getClass)
+class Application(clusterSharding: ClusterSharding, cosConfig: CosConfig) {
+  self =>
+  private[this] var server: Server = null
+  final val log: Logger = LoggerFactory.getLogger(getClass)
 
-  // start kamon
-  Kamon.init()
+  implicit private val askTimeout: Timeout = cosConfig.askTimeout
 
-  // reflect encryption from config
-  override def protoEncryption: Option[ProtoEncryption] = EncryptionSetting(config).encryption
-
-  // Initialize Plugin Manager
-  lazy val pluginManager: PluginManager = PluginManager.getPlugins(config)
-
-  // let us wire up the handler settings
-  // this will break the application bootstrapping if the handler settings env variables are not set
-  lazy val handlerSetting: HandlerSetting = HandlerSetting(config)
-
-  lazy val loggingAdapter: LoggingAdapter = akka.event.Logging(actorSystem.classicSystem, this.getClass)
-
-  lazy val aggregateRoot: Aggregate = {
-    // let us wire up the writeSide executor context
-    val writeSideHandlerServiceClient: WriteSideHandlerServiceClient = {
-      val writeSideExecutionContext: MessageDispatcher =
-        actorSystem.dispatchers.lookup(handlerSetting.writeSideDispatcher)
-
-      val writeClientSettings: GrpcClientSettings =
-        GrpcClientSettings.fromConfig("chief_of_state.v1.WriteSideHandlerService")(actorSystem)
-
-      WriteSideHandlerServiceClient(writeClientSettings)(writeSideExecutionContext, loggingAdapter)
-    }
-
-    //  Register a shutdown task to release resources of the client
-    coordinatedShutdown
-      .addTask(CoordinatedShutdown.PhaseServiceUnbind, "shutdown-writeSidehandler-service-client") { () =>
-        writeSideHandlerServiceClient.close()
-      }
-
-    // wire up the various event and command handler
-    val eventHandler: EventHandler = new AggregateEventHandler(writeSideHandlerServiceClient, handlerSetting)
-    val commandHandler: CommandHandler = new AggregateCommandHandler(writeSideHandlerServiceClient, handlerSetting)
-
-    new Aggregate(
-      actorSystem,
-      config,
-      commandHandler,
-      eventHandler,
-      encryptionAdapter
-    )
-  }
-
-  lazy val server: LagomServer = {
-    // get the SendCommandSettings for the GrpcServiceImpl
-    val sendCommandSettings: SendCommandSettings = SendCommandSettings(config)
-
-    val restService: RestServiceImpl =
-      new RestServiceImpl(clusterSharding, persistentEntityRegistry, aggregateRoot)
-
-    val grpcService: GrpcServiceImpl =
-      new GrpcServiceImpl(actorSystem, clusterSharding, aggregateRoot, sendCommandSettings, pluginManager)
-
-    serverFor[ChiefOfStateService](restService)
-      .additionalRouter(grpcService)
-  }
-
-  if (config.getBoolean("chief-of-state.read-model.enabled")) {
-    val readSideExecutionContext = actorSystem.dispatchers.lookup(handlerSetting.readSideDispatcher)
-
-    // wiring up the grpc for the readSide client
-    ReadSideSetting.getReadSideSettings.foreach { config =>
-      val readSideHandlerServiceClient: ReadSideHandlerServiceClient =
-        ReadSideHandlerServiceClient(config.getGrpcClientSettings(actorSystem))(readSideExecutionContext,
-                                                                                loggingAdapter
+  /**
+   * start the grpc server
+   */
+  private def start(): Unit = {
+    server = NettyServerBuilder
+      .forPort(cosConfig.grpcConfig.server.port)
+      .addService(
+        ServerInterceptors.intercept(
+          ChiefOfStateService.bindService(new GrpcServiceImpl(clusterSharding), ExecutionContext.global),
+          GrpcHeadersInterceptor
         )
+      )
+      .build()
+      .start()
 
-      coordinatedShutdown.addTask(
-        CoordinatedShutdown.PhaseServiceUnbind,
-        s"shutdown-readSidehandler-service-client-${config.processorId}"
-      ) { () =>
-        readSideHandlerServiceClient.close()
-      }
+    log.info("[ChiefOfState] gRPC Server started, listening on " + cosConfig.grpcConfig.server.port)
 
-      // explicit initialization so that we can pass the desired execution context
-      lazy val readSideProcessor: ReadProcessor =
-        new ReadProcessor(config, encryptionAdapter, actorSystem, readSideHandlerServiceClient)(
-          readSideExecutionContext
-        )
-      readSideProcessor.start()
+    sys.addShutdownHook {
+      self.stop()
     }
   }
 
-  this.startAggregateRootCluster()
+  /**
+   * stops the grp server
+   */
+  private def stop(): Unit = {
+    if (server != null) {
+      log.info("*** shutting down gRPC server since JVM is shutting down")
+      server.shutdown()
+    }
+  }
+
+  private def blockUntilShutdown(): Unit = {
+    if (server != null) {
+      server.awaitTermination()
+    }
+  }
 }
 
-/**
- * ApplicationLoader boostraps the application at runtime
- */
-class ApplicationLoader extends LagomApplicationLoader {
-  override def load(context: LagomApplicationContext): LagomApplication =
-    new Application(context) with AkkaDiscoveryComponents
+object Application extends App {
 
-  override def loadDevMode(context: LagomApplicationContext): LagomApplication =
-    new Application(context) with LagomDevModeComponents
+  // Application config
+  val config: Config = ConfigFactory.load().resolve()
 
-  override def describeService: Option[Descriptor] = Some(readDescriptor[ChiefOfStateService])
+  // load the main application config
+  val cosConfig: CosConfig = CosConfig(config)
+
+  // boot the actor system
+  val actorSystem: ActorSystem[Nothing] =
+    ActorSystem[Nothing](Behaviors.empty, "ChiefOfStateSystem", config)
+
+  // instance of the clusterSharding
+  val sharding: ClusterSharding = ClusterSharding(actorSystem)
+
+  val channel: ManagedChannel =
+    NettyChannelBuilder
+      .forAddress(cosConfig.writeSideConfig.host, cosConfig.writeSideConfig.port)
+      .usePlaintext()
+      .build()
+
+  val writeHandler: WriteSideHandlerServiceBlockingStub = new WriteSideHandlerServiceBlockingStub(channel)
+  val remoteCommandHandler: RemoteCommandHandler = RemoteCommandHandler(cosConfig.grpcConfig, writeHandler)
+  val remoteEventHandler: RemoteEventHandler = RemoteEventHandler(cosConfig.grpcConfig, writeHandler)
+
+  // registration at startup
+  sharding.init(Entity(typeKey = AggregateRoot.TypeKey) { entityContext =>
+
+    val shardIndex: Int = Math.abs(entityContext.entityId.hashCode) % cosConfig.eventsConfig.numShards
+
+    AggregateRoot(
+      PersistenceId(entityContext.entityTypeKey.name, entityContext.entityId),
+      shardIndex,
+      cosConfig,
+      remoteCommandHandler,
+      remoteEventHandler
+    )
+  })
+
+  // Akka Management hosts the HTTP routes used by bootstrap
+  AkkaManagement(actorSystem).start()
+
+  // Starting the bootstrap process needs to be done explicitly
+  ClusterBootstrap(actorSystem).start()
+
+  // read side settings
+  if (cosConfig.enableReadSide && ReadSideConfigFactory.getReadSideSettings.nonEmpty) {
+    ReadSideConfigFactory.getReadSideSettings.foreach(rsconfig => {
+      val channel: ManagedChannel =
+        NettyChannelBuilder
+          .forAddress(rsconfig.host.get, rsconfig.port.get)
+          .usePlaintext()
+          .build()
+
+      val rpcClient: ReadSideHandlerServiceBlockingStub = new ReadSideHandlerServiceBlockingStub(channel)
+      val remoteReadSideProcessor: RemoteReadSideProcessor = new RemoteReadSideProcessor(rpcClient)
+      val readSideProcessor: ReadSideProcessor =
+        new ReadSideProcessor(actorSystem, rsconfig.processorId, remoteReadSideProcessor, cosConfig)
+
+      readSideProcessor.init()
+    })
+  }
+
+  // start the gRPC server
+  val server: Application = new Application(sharding, cosConfig)
+  server.start()
+  server.blockUntilShutdown()
 }
