771a74bc87d75737d8b282d83ca53faa8f0caa94
Arsene Tochemey Gandote <tochemey@hotmail.com>
2020-10-31 20:11:50 +0000

    Complete unit tests for CosConfig

---
+++ code/service/src/test/scala/com/namely/chiefofstate/test/config/CosConfigSpec.scala
@@ -0,0 +1,93 @@
+package com.namely.chiefofstate.test.config
+
+import com.namely.chiefofstate.config.CosConfig
+import com.namely.chiefofstate.test.helper.BaseSpec
+import com.typesafe.config.{Config, ConfigException, ConfigFactory}
+
+class CosConfigSpec extends BaseSpec {
+  "Loading main config" should {
+    "be successful when all settings are set" in {
+      val config: Config = ConfigFactory.parseString(s"""
+            akka.cluster.sharding.number-of-shards = 2
+            chiefofstate {
+             	service-name = "chiefofstate"
+              ask-timeout = 5
+              snapshot-criteria {
+                disable-snapshot = false
+                retention-frequency = 100
+                retention-number = 2
+                delete-events-on-snapshot = false
+              }
+              events {
+                tagname: "cos"
+              }
+              grpc {
+                client {
+                  deadline-timeout = 100
+                }
+                server {
+                  port = 9000
+                }
+              }
+              write-side {
+                host = "localhost"
+                port = 1000
+                enable-protos-validation = false
+                states-protos = ""
+                events-protos = ""
+              }
+              read-side {
+                create-stores {
+                  auto = true
+                }
+                # set this value to true whenever a readSide config is set
+                enabled = false
+              }
+            }
+          """)
+      noException shouldBe thrownBy(CosConfig(config))
+    }
+
+    "fail when there is either a missing setting or a wrong naming" in {
+      val config: Config = ConfigFactory.parseString(s"""
+            akka.cluster.sharding.number-of-shards = 2
+            chiefofstate {
+             	service-name = "chiefofstate"
+              ask-timeouts = 5 # wrong naming
+              snapshot-criteria {
+                disable-snapshot = false
+                retention-frequency = 100
+                retention-number = 2
+                delete-events-on-snapshot = false
+              }
+              events {
+                tagname: "cos"
+              }
+              grpc {
+                client {
+                  deadline-timeout = 100
+                }
+                server {
+                  port = 9000
+                }
+              }
+              write-side {
+                host = "localhost"
+                port = 1000
+                enable-protos-validation = false
+                states-protos = ""
+                events-protos = ""
+              }
+              read-side {
+                create-stores {
+                  auto = true
+                }
+                # set this value to true whenever a readSide config is set
+                enabled = false
+              }
+            }
+          """)
+      an[ConfigException] shouldBe thrownBy(CosConfig(config))
+    }
+  }
+}
