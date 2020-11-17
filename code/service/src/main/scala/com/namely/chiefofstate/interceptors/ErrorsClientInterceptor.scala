f87f2dcd51f4b1de12f62d08584469a4f58d1c91
zen <zen.yui@namely.com>
2020-11-17 15:24:17 -0500

    scalafmt

--- code/service/src/main/scala/com/namely/chiefofstate/interceptors/ErrorsClientInterceptor.scala
+++ code/service/src/main/scala/com/namely/chiefofstate/interceptors/ErrorsClientInterceptor.scala
@@ -11,22 +11,22 @@ import io.grpc.Metadata
 import io.grpc.ClientCall.Listener
 import io.opentracing.Span
 
-
 /**
-  * gRPC client intercpetor for ensuring remote errors are logged
-  * on the current span
-  *
-  * @param tracer the tracer, likely the GlobalGracer
-  */
+ * gRPC client intercpetor for ensuring remote errors are logged
+ * on the current span
+ *
+ * @param tracer the tracer, likely the GlobalGracer
+ */
 class ErrorsClientInterceptor(tracer: Tracer) extends ClientInterceptor {
+
   /**
-    * intercept the call and apply the custom listener
-    *
-    * @param method the method to intercept
-    * @param callOptions the call options
-    * @param next the channel
-    * @return a client call wrapped in the listener
-    */
+   * intercept the call and apply the custom listener
+   *
+   * @param method the method to intercept
+   * @param callOptions the call options
+   * @param next the channel
+   * @return a client call wrapped in the listener
+   */
   override def interceptCall[T, U](method: MethodDescriptor[T, U],
                                    callOptions: CallOptions,
                                    next: Channel
@@ -38,48 +38,49 @@ class ErrorsClientInterceptor(tracer: Tracer) extends ClientInterceptor {
 }
 
 /**
-  * companion object with the custom client call and listener classes
-  */
+ * companion object with the custom client call and listener classes
+ */
 object ErrorsClientInterceptor {
   final val logger: Logger = LoggerFactory.getLogger(getClass)
 
   /**
-    * custom client call class for applying the custom listener
-    *
-    * @param call a call to intercept
-    * @param tracer the tracer to use
-    */
+   * custom client call class for applying the custom listener
+   *
+   * @param call a call to intercept
+   * @param tracer the tracer to use
+   */
   class CustomClientCall[T, U](call: ClientCall[T, U], tracer: Tracer) extends SimpleForwardingClientCall[T, U](call) {
+
     /**
-      * on start of a call, apply a custom reponse listener
-      *
-      * @param responseListener a custom response listener
-      * @param headers the headers of the call
-      */
+     * on start of a call, apply a custom reponse listener
+     *
+     * @param responseListener a custom response listener
+     * @param headers the headers of the call
+     */
     override def start(responseListener: Listener[U], headers: Metadata): Unit = {
       super.start(new CustomListener[U](tracer, responseListener), headers)
     }
   }
 
   /**
-    * custom listener class for intercepting the call and propagaing
-    * non-ok statuses as errors into the tracer
-    *
-    * @param tracer a tracer to report to
-    * @param listener a listener to wrap
-    */
+   * custom listener class for intercepting the call and propagaing
+   * non-ok statuses as errors into the tracer
+   *
+   * @param tracer a tracer to report to
+   * @param listener a listener to wrap
+   */
   class CustomListener[U](tracer: Tracer, listener: ClientCall.Listener[U])
       extends SimpleForwardingClientCallListener[U](listener) {
 
     val span: Option[Span] = Option(tracer.activeSpan())
 
     /**
-      * on close of the call, report a non-ok status as an error
-      * to the tracer
-      *
-      * @param status the call status
-      * @param trailers trailing metadata
-      */
+     * on close of the call, report a non-ok status as an error
+     * to the tracer
+     *
+     * @param status the call status
+     * @param trailers trailing metadata
+     */
     override def onClose(status: Status, trailers: Metadata): Unit = {
       if (!status.isOk()) {
         logger.debug(s"${status.isOk()} ${status.getCode()} ${status.getDescription()}")
--- code/service/src/main/scala/com/namely/chiefofstate/interceptors/ErrorsServerInterceptor.scala
+++ code/service/src/main/scala/com/namely/chiefofstate/interceptors/ErrorsServerInterceptor.scala
@@ -11,19 +11,20 @@ import io.opentracing.Tracer
 import io.opentracing.Span
 
 /**
-  * custom server gRPC interceptor for propagating errors to the tracer
-  *
-  * @param tracer the tracer to use, likely GlobalGracer
-  */
+ * custom server gRPC interceptor for propagating errors to the tracer
+ *
+ * @param tracer the tracer to use, likely GlobalGracer
+ */
 class ErrorsServerInterceptor(tracer: Tracer) extends ServerInterceptor {
+
   /**
-    * intercepts a call and starts the custom caller
-    *
-    * @param call a call to intercept
-    * @param headers the metadata
-    * @param next the call handler
-    * @return a listener
-    */
+   * intercepts a call and starts the custom caller
+   *
+   * @param call a call to intercept
+   * @param headers the metadata
+   * @param next the call handler
+   * @return a listener
+   */
   override def interceptCall[T, U](call: ServerCall[T, U],
                                    headers: Metadata,
                                    next: ServerCallHandler[T, U]
@@ -42,24 +43,23 @@ class ErrorsServerInterceptor(tracer: Tracer) extends ServerInterceptor {
 object ErrorsServerInterceptor {
   final val logger: Logger = LoggerFactory.getLogger(getClass)
 
-
   /**
-    * custom caller class for logging errors on close of the call
-    *
-    * @param tracer the tracer to report to
-    * @param call a call to intercept
-    */
+   * custom caller class for logging errors on close of the call
+   *
+   * @param tracer the tracer to report to
+   * @param call a call to intercept
+   */
   class CustomCaller[T, U](tracer: Tracer, call: ServerCall[T, U]) extends SimpleForwardingServerCall[T, U](call) {
 
     val span: Option[Span] = Option(tracer.activeSpan())
 
     /**
-      * on close of the RPC, report any non-ok statuses
-      * as errors to the tracer.
-      *
-      * @param status the status of the call
-      * @param trailers the trailing metadata
-      */
+     * on close of the RPC, report any non-ok statuses
+     * as errors to the tracer.
+     *
+     * @param status the status of the call
+     * @param trailers the trailing metadata
+     */
     override def close(status: Status, trailers: Metadata): Unit = {
       if (!status.isOk()) {
         OpentracingHelpers.reportErrorToSpan(span, status.asException())
