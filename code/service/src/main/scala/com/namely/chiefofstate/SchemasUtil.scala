a8c374547e2cbf9356853f5a80f6e33e83ddbd1d
Arsene Tochemey Gandote <tochemey@hotmail.com>
2021-01-26 23:23:09 +0000

    Ready to go :tada:

--- code/service/src/main/scala/com/namely/chiefofstate/SchemasUtil.scala
+++ code/service/src/main/scala/com/namely/chiefofstate/SchemasUtil.scala
@@ -12,122 +12,12 @@ import slick.basic.DatabaseConfig
 import slick.jdbc.PostgresProfile
 
 import java.sql.{Connection, Statement}
-import scala.annotation.unused
 
 /**
  * Utility class to create the necessary schemas for the write side
  */
 object SchemasUtil {
   final val log: Logger = LoggerFactory.getLogger(getClass)
-
-  // $COVERAGE-OFF$
-
-  // FIXME the following lines will be used once a migration tool is ready
-
-  /**
-   * journalTableStatement returns the journal ddl
-   *
-   * @return the sql statement
-   */
-  private def journalTableStatement(): Seq[String] = {
-    Seq(
-      s"""
-        CREATE TABLE IF NOT EXISTS event_journal(
-          ordering BIGSERIAL,
-          persistence_id VARCHAR(255) NOT NULL,
-          sequence_number BIGINT NOT NULL,
-          deleted BOOLEAN DEFAULT FALSE NOT NULL,
-
-          writer VARCHAR(255) NOT NULL,
-          write_timestamp BIGINT,
-          adapter_manifest VARCHAR(255),
-
-          event_ser_id INTEGER NOT NULL,
-          event_ser_manifest VARCHAR(255) NOT NULL,
-          event_payload BYTEA NOT NULL,
-
-          meta_ser_id INTEGER,
-          meta_ser_manifest VARCHAR(255),
-          meta_payload BYTEA,
-
-          PRIMARY KEY(persistence_id, sequence_number)
-        );""",
-      // create index
-      s"""CREATE UNIQUE INDEX event_journal_ordering_idx ON event_journal(ordering);""",
-      // create the event_tag
-      s"""
-        CREATE TABLE IF NOT EXISTS event_tag(
-            event_id BIGINT,
-            tag VARCHAR(256),
-            PRIMARY KEY(event_id, tag),
-            CONSTRAINT fk_event_journal
-              FOREIGN KEY(event_id)
-              REFERENCES event_journal(ordering)
-              ON DELETE CASCADE
-        );
-      """
-    )
-  }
-
-  /**
-   * snapshotTableStatement return the snapshot ddl statement
-   *
-   * @return the sql statement
-   */
-  private def snapshotTableStatement(): String = {
-    s"""
-     CREATE TABLE IF NOT EXISTS snapshot (
-      persistence_id VARCHAR(255) NOT NULL,
-      sequence_number BIGINT NOT NULL,
-      created BIGINT NOT NULL,
-
-      snapshot_ser_id INTEGER NOT NULL,
-      snapshot_ser_manifest VARCHAR(255) NOT NULL,
-      snapshot_payload BYTEA NOT NULL,
-
-      meta_ser_id INTEGER,
-      meta_ser_manifest VARCHAR(255),
-      meta_payload BYTEA,
-
-      PRIMARY KEY(persistence_id, sequence_number)
-     )"""
-  }
-
-  /**
-   *  Attempts to create the various write side  data stores
-   */
-  @unused
-  private def createSchemas(config: Config): Boolean = {
-    val dc: DatabaseConfig[PostgresProfile] =
-      DatabaseConfig.forConfig[PostgresProfile]("write-side-slick", config)
-
-    val journalSQLs: Seq[String] = journalTableStatement()
-    val snapshotSQL: String = snapshotTableStatement()
-
-    val conn: Connection = dc.db.createSession().conn
-
-    try {
-      val stmt: Statement = conn.createStatement()
-      try {
-        log.info("setting up journal and snapshot stores....")
-        // create the journal table and snapshot journal
-        // if DDLs failed, it will raise an SQLException
-        journalSQLs
-          .map(stmt.execute)
-          .map(_ => stmt.execute(snapshotSQL))
-          .forall(identity)
-
-      } finally {
-        stmt.close()
-      }
-    } finally {
-      log.info("journal and snapshot stores setup. Releasing resources....")
-      conn.close()
-      dc.db.close()
-    }
-  }
-
-  // $COVERAGE-ON$
 
   /**
    * legacyJournalStatement returns the journal ddl
