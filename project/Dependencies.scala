9c4b3da3849f9c45855f70a2bb5150125085dda9
Scala Steward <me@scala-steward.org>
2021-02-18 09:00:35 +0100

    Update scalatest to 3.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val LogbackVersion: String = "1.2.3"
     val ScalapbCommonProtoVersion: String = "1.18.1-1"
     val ScalapbValidationVersion: String = "0.1.4"
-    val ScalaTestVersion: String = "3.2.3"
+    val ScalaTestVersion: String = "3.2.4"
     val AkkaManagementVersion: String = "1.0.9"
     val AkkaProjectionVersion: String = "1.1.0"
     val PostgresDriverVersion: String = "42.2.18"
