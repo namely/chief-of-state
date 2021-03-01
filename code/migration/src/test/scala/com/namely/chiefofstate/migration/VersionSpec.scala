36b50dc82a3373ac964ee8d2334030872ed4df03
Zen Yui <zen.yui@namely.com>
2021-02-28 19:30:56 -0500

    Issue-197: add db migration tools (#228)

---
+++ code/migration/src/test/scala/com/namely/chiefofstate/migration/VersionSpec.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 Namely Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.namely.chiefofstate.migration
+
+import scala.util.{Success, Try}
+import slick.dbio.{DBIO, DBIOAction}
+
+class VersionSpec extends BaseSpec {
+
+  case class SomeVersion(versionNumber: Int) extends Version {
+    def snapshot(): DBIO[Unit] = DBIOAction.successful {}
+    def upgrade(): DBIO[Unit] = DBIOAction.successful {}
+  }
+
+  "VersionOrdering" should {
+    "order versions by version number" in {
+      val versionOne = SomeVersion(1)
+      val versionTwo = SomeVersion(2)
+
+      val expectedCompare = versionOne.versionNumber.compare(versionTwo.versionNumber)
+
+      Version.VersionOrdering.compare(versionOne, versionTwo) shouldBe expectedCompare
+    }
+  }
+
+}
