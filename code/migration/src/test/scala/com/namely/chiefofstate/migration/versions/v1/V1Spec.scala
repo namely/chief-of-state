46215ca9ada512c3861b19ebec387bc1657bc430
zen <zen.yui@namely.com>
2021-03-03 02:29:01 -0500

    add test scaffolding

---
+++ code/migration/src/test/scala/com/namely/chiefofstate/migration/versions/v1/V1Spec.scala
@@ -0,0 +1,69 @@
+package com.namely.chiefofstate.migration.versions.v1
+
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import com.typesafe.config.Config
+import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
+import org.testcontainers.utility.DockerImageName
+import java.sql.DriverManager
+import java.sql.Connection
+
+class V1Spec extends BaseSpec with ForAllTestContainer {
+
+  val testKit: ActorTestKit = ActorTestKit()
+
+  val cosSchema: String = "cos"
+
+  override val container: PostgreSQLContainer = PostgreSQLContainer
+    .Def(
+      dockerImageName = DockerImageName.parse("postgres"),
+      databaseName = "postgres",
+      urlParams = Map("currentSchema" -> cosSchema)
+    )
+    .createContainer()
+
+  /** create connection to the container db for test statements */
+  def getConnection(): Connection = {
+    // load the driver
+    Class.forName("org.postgresql.Driver")
+
+    DriverManager
+      .getConnection(container.jdbcUrl, container.username, container.password)
+  }
+
+  def recreateSchema(): Unit = {
+    val statement = getConnection().createStatement()
+    statement.addBatch(s"drop schema if exists $cosSchema cascade")
+    statement.addBatch(s"create schema $cosSchema")
+    statement.executeBatch()
+  }
+
+  override def beforeEach() = {
+    super.beforeEach()
+    recreateSchema()
+  }
+
+  override protected def afterAll() = {
+    super.afterAll()
+    testKit.shutdownTestKit()
+    clearEnv()
+  }
+
+  ".beforeUpgrade" should {
+    "migrate data into the new table" in {
+      // TODO
+    }
+  }
+  ".afterUpgrade" should {
+    "drop the table on the old connection" in {
+      // TODO
+    }
+    "do nothing if on the new connection" in {
+      // TODO
+    }
+  }
+  ".upgrade" should {
+    "drop the old table and index" in {
+      // TODO
+    }
+  }
+}
---
+++ code/migration/src/test/scala/com/namely/chiefofstate/migration/versions/v2/V2Spec.scala
@@ -0,0 +1,52 @@
+package com.namely.chiefofstate.migration.versions.v2
+
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import com.typesafe.config.Config
+import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
+import org.testcontainers.utility.DockerImageName
+import java.sql.DriverManager
+import java.sql.Connection
+
+class V2Spec extends BaseSpec with ForAllTestContainer {
+
+  val testKit: ActorTestKit = ActorTestKit()
+
+  val cosSchema: String = "cos"
+
+  override val container: PostgreSQLContainer = PostgreSQLContainer
+    .Def(
+      dockerImageName = DockerImageName.parse("postgres"),
+      databaseName = "postgres",
+      urlParams = Map("currentSchema" -> cosSchema)
+    )
+    .createContainer()
+
+  /** create connection to the container db for test statements */
+  def getConnection(): Connection = {
+    // load the driver
+    Class.forName("org.postgresql.Driver")
+
+    DriverManager
+      .getConnection(container.jdbcUrl, container.username, container.password)
+  }
+
+  def recreateSchema(): Unit = {
+    val statement = getConnection().createStatement()
+    statement.addBatch(s"drop schema if exists $cosSchema cascade")
+    statement.addBatch(s"create schema $cosSchema")
+    statement.executeBatch()
+  }
+
+  override def beforeEach() = {
+    super.beforeEach()
+    recreateSchema()
+  }
+
+  override protected def afterAll() = {
+    super.afterAll()
+    testKit.shutdownTestKit()
+    clearEnv()
+  }
+
+  // TODO: add tests
+}
