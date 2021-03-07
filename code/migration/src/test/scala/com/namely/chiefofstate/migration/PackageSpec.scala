5223471cc06cc3d2d607c651f05f603720ed0522
Arsene Tochemey Gandote <tochemey@hotmail.com>
2021-03-07 13:07:29 +0000

    Handle read side offset store table name case (#239)

---
+++ code/migration/src/test/scala/com/namely/chiefofstate/migration/PackageSpec.scala
@@ -0,0 +1,28 @@
+package com.namely.chiefofstate.migration
+
+class PackageSpec extends BaseSpec {
+
+  ".isUpper" should {
+    "be true" in {
+      "AKKA_PROJECTION_OFFSET_STORE".isUpper shouldBe true
+      "AKKA PROJECTION OFFSET STORE".isUpper shouldBe true
+    }
+
+    "be false with mixed cases" in {
+      val tableName: String = "AKKA_read"
+      tableName.isUpper shouldBe false
+    }
+
+    "be false lowercase" in {
+      val tableName: String = "read_side_offset"
+      tableName.isUpper shouldBe false
+    }
+  }
+
+  ".quote" should {
+    "work as expected" in {
+      val result: String = "AKKA_PROJECTION_OFFSET_STORE".quote
+      result shouldBe s"""\"AKKA_PROJECTION_OFFSET_STORE\""""
+    }
+  }
+}
