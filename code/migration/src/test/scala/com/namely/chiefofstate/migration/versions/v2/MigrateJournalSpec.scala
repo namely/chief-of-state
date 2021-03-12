b878a272c4a5fe6d85ff1b1f732f517ca1533a2d
Arsene Tochemey Gandote <tochemey@hotmail.com>
2021-03-12 10:54:31 +0000

    MigrateJournal Test

--- code/migration/src/test/scala/com/namely/chiefofstate/migration/versions/v2/MigrateJournalSpec.scala
+++ code/migration/src/test/scala/com/namely/chiefofstate/migration/versions/v2/MigrateJournalSpec.scala
@@ -7,22 +7,24 @@
 package com.namely.chiefofstate.migration.versions.v2
 
 import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.typed.ActorSystem
 import akka.persistence.jdbc.config.JournalConfig
-import akka.persistence.jdbc.journal.dao.legacy
-import akka.persistence.jdbc.journal.dao.legacy.JournalRow
+import akka.persistence.jdbc.db.SlickExtension
+import akka.persistence.jdbc.journal.dao.{legacy, JournalQueries}
+import akka.persistence.jdbc.journal.dao.legacy.ByteArrayJournalDao
 import akka.serialization.{Serialization, SerializationExtension}
 import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
 import com.github.dockerjava.api.command.CreateContainerCmd
 import com.github.dockerjava.api.model.{ExposedPort, PortBinding, Ports}
 import com.namely.chiefofstate.migration.{BaseSpec, JdbcConfig}
-import com.namely.protobuf.chiefofstate.v1.tests.{AccountDebited, AccountOpened}
+import com.namely.chiefofstate.migration.versions.v2.Seeding.insertNonSerializedDataIntoLegacy
 import com.typesafe.config.{Config, ConfigFactory}
 import slick.basic.DatabaseConfig
-import slick.jdbc.JdbcProfile
+import slick.jdbc.{JdbcBackend, JdbcProfile}
 import slick.jdbc.PostgresProfile.api._
 
 import java.sql.{Connection, DriverManager}
-import scala.concurrent.Await
+import scala.concurrent.{Await, ExecutionContextExecutor}
 import scala.concurrent.duration.Duration
 
 class MigrateJournalSpec extends BaseSpec with ForAllTestContainer {
@@ -68,108 +70,6 @@ class MigrateJournalSpec extends BaseSpec with ForAllTestContainer {
 
   lazy val testKit: ActorTestKit = ActorTestKit(config)
 
-  def insertNonSerializedDataIntoLegacy(
-    journalJdbcConfig: DatabaseConfig[JdbcProfile],
-    legacyJournalQueries: legacy.JournalQueries
-  ): Long = {
-
-    val data: Seq[JournalRow] = Seq(
-      JournalRow(
-        1L,
-        false,
-        "some-persistence-id-1",
-        2L,
-        AccountOpened()
-          .withAccountNumber("number-1")
-          .withAccountUuid("some-persistence-id-1")
-          .toByteArray,
-        Some("chiefofstate0")
-      ),
-      JournalRow(
-        2L,
-        false,
-        "some-persistence-id-2",
-        4L,
-        AccountOpened()
-          .withAccountNumber("number-2")
-          .withAccountUuid("some-persistence-id-2")
-          .toByteArray,
-        Some("chiefofstate1")
-      ),
-      JournalRow(
-        3L,
-        false,
-        "some-persistence-id-3",
-        12L,
-        AccountOpened()
-          .withAccountNumber("number-3")
-          .withAccountUuid("some-persistence-id-3")
-          .toByteArray,
-        Some("chiefofstate0")
-      ),
-      JournalRow(
-        4L,
-        false,
-        "some-persistence-id-0",
-        6L,
-        AccountOpened()
-          .withAccountNumber("number-0")
-          .withAccountUuid("some-persistence-id-0")
-          .toByteArray,
-        Some("chiefofstate0")
-      ),
-      JournalRow(
-        5L,
-        false,
-        "some-persistence-id-0",
-        8L,
-        AccountDebited()
-          .withBalance(10)
-          .withAccountUuid("some-persistence-id-0")
-          .toByteArray,
-        Some("chiefofstate2")
-      ),
-      JournalRow(
-        6L,
-        false,
-        "some-persistence-id-0",
-        20L,
-        AccountDebited()
-          .withBalance(20)
-          .withAccountUuid("some-persistence-id-0")
-          .toByteArray,
-        Some("chiefofstate2")
-      ),
-      JournalRow(
-        7L,
-        false,
-        "some-persistence-id-3",
-        2L,
-        AccountDebited()
-          .withBalance(20)
-          .withAccountUuid("some-persistence-id-3")
-          .toByteArray,
-        Some("chiefofstate3")
-      ),
-      JournalRow(
-        8L,
-        false,
-        "some-persistence-id-2",
-        2L,
-        AccountDebited()
-          .withBalance(20)
-          .withAccountUuid("some-persistence-id-2")
-          .toByteArray,
-        Some("chiefofstate0")
-      )
-    )
-
-    val inserts = legacyJournalQueries.JournalTable
-      .returning(legacyJournalQueries.JournalTable.map(_.ordering)) ++= data
-
-    Await.result(journalJdbcConfig.db.run(inserts), Duration.Inf).length
-  }
-
   def countLegacyJournal(journalJdbcConfig: DatabaseConfig[JdbcProfile],
                          legacyJournalQueries: legacy.JournalQueries
   ): Int = {
@@ -186,7 +86,7 @@ class MigrateJournalSpec extends BaseSpec with ForAllTestContainer {
     testKit.shutdownTestKit()
   }
 
-  "With MigrateJournal we" should {
+  "MigrateJournal" should {
     "create legacy tables and insert data onto the new journal" in {
       val journalJdbcConfig: DatabaseConfig[JdbcProfile] = JdbcConfig.journalConfig(config)
       val profile: JdbcProfile = journalJdbcConfig.profile
@@ -208,8 +108,54 @@ class MigrateJournalSpec extends BaseSpec with ForAllTestContainer {
       // let us test the next ordering value
       migrator.nextOrderingValue() shouldBe 9L
     }
+
     "migrate old data into the new journal schema" in {
-      // TODO
+      implicit val ec: ExecutionContextExecutor = testKit.system.executionContext
+      implicit val sys: ActorSystem[Nothing] = testKit.system
+
+      val journalJdbcConfig: DatabaseConfig[JdbcProfile] = JdbcConfig.journalConfig(config)
+      val profile: JdbcProfile = journalJdbcConfig.profile
+      val journalConfig: JournalConfig = new JournalConfig(config.getConfig("jdbc-journal"))
+      val legacyJournalQueries: legacy.JournalQueries =
+        new legacy.JournalQueries(profile, journalConfig.journalTableConfiguration)
+
+      val newJournalQueries: JournalQueries =
+        new JournalQueries(profile,
+                           journalConfig.eventJournalTableConfiguration,
+                           journalConfig.eventTagTableConfiguration
+        )
+
+      val serialization: Serialization = SerializationExtension(testKit.system)
+      val migrator: MigrateJournal = MigrateJournal(testKit.system, profile, serialization)
+
+      val journaldb: JdbcBackend.Database =
+        SlickExtension(testKit.system).database(config.getConfig("jdbc-read-journal")).database
+
+      val legacyDao: ByteArrayJournalDao = new ByteArrayJournalDao(journaldb, profile, journalConfig, serialization)
+
+      // let us create the legacy tables
+      SchemasUtil.createLegacyJournalAndSnapshot(journalJdbcConfig) shouldBe {}
+
+      // let us seed some data into the legacy journal
+      noException shouldBe thrownBy(Seeding.seedLegacyJournal(legacyDao))
+
+      // let us count the old journal
+      countLegacyJournal(journalJdbcConfig, legacyJournalQueries) shouldBe 6
+
+      // let us test the next ordering value
+      migrator.nextOrderingValue() shouldBe 7L
+
+      // let us create the new journal table
+      SchemasUtil.createJournalTables(journalJdbcConfig) shouldBe {}
+
+      // let us migrate the data
+      migrator.migrateWithBatchSize() shouldBe {}
+
+      // let us get the number of records in the new journal
+      Await.result(journalJdbcConfig.db
+                     .run(newJournalQueries.JournalTable.map(_.ordering).length.result),
+                   Duration.Inf
+      ) shouldBe 6
     }
   }
 }
