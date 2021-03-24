a3aba76bddc68ed2e14dba8bfe0d9025c60be8e8
Kelvin Mungai <klvmaina@gmail.com>
2021-03-24 15:06:08 +0300

    Add custom interceptors to add missing attributes

---
+++ code/service/src/main/scala/com/namely/chiefofstate/telemetry/StatusClientInterceptor.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 Namely Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.namely.chiefofstate.telemetry
+
+import io.grpc.ForwardingClientCall.SimpleForwardingClientCall
+import io.grpc.ForwardingClientCallListener.SimpleForwardingClientCallListener
+import io.grpc.{CallOptions, Channel, ClientCall, ClientInterceptor, Metadata, MethodDescriptor, Status}
+import io.opentelemetry.api.trace.Span
+
+/**
+ * gRPC client interceptor that adds the returned gRPC status code to the trace attributes
+ */
+class StatusClientInterceptor extends ClientInterceptor {
+  override def interceptCall[ReqT, RespT](method: MethodDescriptor[ReqT, RespT],
+                                          callOptions: CallOptions,
+                                          next: Channel
+  ): ClientCall[ReqT, RespT] = {
+    new StatusClientInterceptor.CustomClientCall(next.newCall(method, callOptions))
+  }
+}
+
+object StatusClientInterceptor {
+  val GrpcStatusLabel = "grpc.status_code"
+
+  val GrpcKindLabel = "grpc.kind"
+
+  val GrpcKind = "client"
+
+  /**
+   * Custom call wrapper that will hook in our custom listener to log returned status codes
+   * @param call client call to be wrapped
+   */
+  class CustomClientCall[T, U](call: ClientCall[T, U]) extends SimpleForwardingClientCall[T, U](call) {
+    override def start(responseListener: ClientCall.Listener[U], headers: Metadata): Unit = {
+      super.start(new CustomListener(responseListener), headers)
+    }
+  }
+
+  class CustomListener[U](listener: ClientCall.Listener[U]) extends SimpleForwardingClientCallListener[U](listener) {
+
+    /**
+     * Custom close logic that reports the status code
+     * @param status gRPC status code for the closed call
+     * @param trailers metadata used in the call
+     */
+    override def onClose(status: Status, trailers: Metadata): Unit = {
+      Span
+        .current()
+        .setAttribute(GrpcStatusLabel, status.getCode.name())
+        .setAttribute(GrpcKindLabel, GrpcKind)
+      super.onClose(status, trailers)
+    }
+  }
+}
