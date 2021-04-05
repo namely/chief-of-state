0f3b3c02937014f7e3519b6c6c167d05b7d3c3b1
zen <zen.yui@namely.com>
2021-04-05 14:50:28 -0400

    fix new wrappers

--- code/migration/src/main/scala/com/namely/chiefofstate/migration/versions/v4/V4.scala
+++ code/migration/src/main/scala/com/namely/chiefofstate/migration/versions/v4/V4.scala
@@ -7,7 +7,7 @@
 package com.namely.chiefofstate.migration.versions.v4
 
 import com.namely.chiefofstate.migration.{SchemasUtil, Version}
-import com.namely.protobuf.chiefofstate.v1.persistence.EventWrapper
+import com.namely.protobuf.chiefofstate.v1.persistence.{EventWrapper, StateWrapper}
 import org.slf4j.{Logger, LoggerFactory}
 import slick.basic.DatabaseConfig
 import slick.dbio.DBIO
@@ -40,13 +40,13 @@ case class V4(
       // update in the journal
       sqlu"""
         UPDATE event_journal
-        SET event_ser_id = #${V4.newSerializerId}, event_ser_manifest = ${V4.newSerializerManifest}
+        SET event_ser_id = #${V4.newSerializerId}, event_ser_manifest = ${V4.newSerializerManifestEvent}
         WHERE event_ser_id = #${V4.oldSerializerId} AND event_ser_manifest = ${V4.oldSerializerManifestEvent}
       """,
       // update the snapshots
       sqlu"""
         UPDATE state_snapshot
-        SET snapshot_ser_id = #${V4.newSerializerId}, snapshot_ser_manifest = ${V4.newSerializerManifest}
+        SET snapshot_ser_id = #${V4.newSerializerId}, snapshot_ser_manifest = ${V4.newSerializerManifestState}
         WHERE snapshot_ser_id = #${V4.oldSerializerId} AND snapshot_ser_manifest = ${V4.oldSerializerManifestState}
       """
     )
@@ -69,5 +69,6 @@ object V4 {
   val oldSerializerManifestEvent: String = "com.namely.protobuf.chiefofstate.v1.persistence.EventWrapper"
   val oldSerializerManifestState: String = "com.namely.protobuf.chiefofstate.v1.persistence.StateWrapper"
   val newSerializerId: Int = 5001
-  val newSerializerManifest: String = EventWrapper.scalaDescriptor.fullName.split("/").last
+  val newSerializerManifestEvent: String = EventWrapper.scalaDescriptor.fullName.split("/").last
+  val newSerializerManifestState: String = StateWrapper.scalaDescriptor.fullName.split("/").last
 }
