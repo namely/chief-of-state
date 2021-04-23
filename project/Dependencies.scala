dcd45ada3bedb0dce0868ab684952bcb068f9055
Scala Steward <me@scala-steward.org>
2021-04-23 02:34:56 +0200

    Update akka-projection-core, ... to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val ScalapbValidationVersion: String = "0.1.4"
     val ScalaTestVersion: String = "3.2.8"
     val AkkaManagementVersion: String = "1.0.10"
-    val AkkaProjectionVersion: String = "1.1.0"
+    val AkkaProjectionVersion: String = "1.2.0"
     val PostgresDriverVersion: String = "42.2.19"
     val SlickVersion: String = "3.3.3"
     val AkkaPersistenceJdbcVersion: String = "5.0.0"
