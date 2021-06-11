97d6f753ad3f39a2649f9363b3ea0f341f521b54
Arsene Tochemey Gandote <tochemey@hotmail.com>
2021-06-11 16:54:54 +0000

    streaming readside initial commit

---
+++ code/service/src/main/scala/com/namely/chiefofstate/readside/ReadSideJdbcStreamHandler.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2020 Namely Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.namely.chiefofstate.readside
+import akka.projection.eventsourced.EventEnvelope
+import akka.projection.jdbc.scaladsl.JdbcHandler
+import akka.projection.jdbc.JdbcSession
+import com.namely.protobuf.chiefofstate.v1.persistence.EventWrapper
+
+/**
+ *  Implements the the akka projection JdbcHandler interface and forwards events to the
+ *  stream readside handler
+ *
+ * @param processorId read side processor id
+ * @param readSideStreamHandler a remote handler implementation
+ */
+private[readside] class ReadSideJdbcStreamHandler(processorId: String, readSideStreamHandler: ReadSideStreamHandler)
+    extends JdbcHandler[Seq[EventEnvelope[EventWrapper]], JdbcSession] {
+
+  override def process(session: JdbcSession, envelopes: Seq[EventEnvelope[EventWrapper]]): Unit = {
+    // construct the list of events to push out
+    val events = envelopes.map(envelope => {
+      (envelope.event.getEvent, envelope.event.getResultingState, envelope.event.getMeta)
+    })
+
+    // send to the remote gRPC stream handler
+    readSideStreamHandler.processEvents(events)
+  }
+}
---
+++ code/service/src/main/scala/com/namely/chiefofstate/readside/ReadSideStreamHandler.scala
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2020 Namely Inc.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.namely.chiefofstate.readside
+import com.google.protobuf.any
+import com.namely.chiefofstate.config.GrpcConfig
+import com.namely.protobuf.chiefofstate.v1.common.MetaData
+import com.namely.protobuf.chiefofstate.v1.readside.{ HandleReadSideRequest, HandleReadSideResponse }
+import com.namely.protobuf.chiefofstate.v1.readside.ReadSideHandlerServiceGrpc.ReadSideHandlerServiceStub
+import io.grpc.stub.StreamObserver
+import io.grpc.Status
+import org.slf4j.{ Logger, LoggerFactory }
+
+import java.util.concurrent.{ CountDownLatch, TimeUnit }
+
+/**
+ *  Processes events read from the Journal by sending them to the read side server
+ *  as gRPC stream
+ */
+private[readside] trait ReadSideStreamHandler {
+
+  /**
+   *  handles a sequence of events that will be used to build a read model
+   *
+   * @param events the sequence of events to handle
+   */
+  def processEvents(events: Seq[(any.Any, any.Any, MetaData)]): Unit
+}
+
+/**
+ * Receives the readside response from an observable stream of messages.
+ *
+ * @param processorId the processor id
+ * @param doneSignal the async signal notification
+ */
+private[readside] case class HandleReadSideResponseStreamObserver(processorId: String, doneSignal: CountDownLatch)
+    extends StreamObserver[HandleReadSideResponse] {
+
+  private val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
+  override def onNext(response: HandleReadSideResponse): Unit = {
+    // onNext will be called only once after the server has finished processing the messages
+    logger.info("received a server response...")
+    if (!response.successful) {
+      val errMsg: String =
+        s"read side streaming message not handled, processor=$processorId"
+      logger.warn(errMsg)
+      throw new RuntimeException(errMsg)
+    }
+  }
+
+  override def onError(t: Throwable): Unit = {
+    val status = Status.fromThrowable(t)
+    val errMsg: String =
+      s"read side streaming returned failure, processor=$processorId, cause=$status"
+    logger.warn(errMsg)
+    doneSignal.countDown()
+  }
+
+  override def onCompleted(): Unit = {
+    // the server is done sending us data
+    // onCompleted will be called right after onNext()
+    doneSignal.countDown()
+  }
+}
+
+/**
+ * read side processor that sends messages to a gRPC server that implements
+ * the ReadSideHandler service
+ *
+ * @param processorId the unique Id for this read side
+ * @param readSideHandlerServiceStub a non-blocking client for a ReadSideHandler
+ */
+private[readside] case class ReadSideStreamHandlerImpl(
+    processorId: String,
+    grpcConfig: GrpcConfig,
+    readSideHandlerServiceStub: ReadSideHandlerServiceStub,
+    doneSignal: CountDownLatch = new CountDownLatch(1))
+    extends ReadSideStreamHandler {
+
+  private val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
+  /**
+   *  handles a sequence of events that will be used to build a read model
+   *
+   * @param events the read event envelopes to handle
+   * @return true or false
+   */
+  override def processEvents(events: Seq[(any.Any, any.Any, MetaData)]): Unit = {
+    // create an instance of the response stream observer
+    val readSideResponseStreamObserver: HandleReadSideResponseStreamObserver =
+      HandleReadSideResponseStreamObserver(processorId = processorId, doneSignal = doneSignal)
+
+    // create the readSide request observer
+    val readSideRequestObserver: StreamObserver[HandleReadSideRequest] =
+      readSideHandlerServiceStub
+        .withDeadlineAfter(grpcConfig.client.timeout, TimeUnit.MILLISECONDS)
+        .handleReadSideStream(readSideResponseStreamObserver)
+
+    try {
+      val it = events.iterator
+      val proceed: Boolean = doneSignal.getCount == 0
+      while (proceed && it.hasNext) {
+        val (event, resultingState, meta) = it.next()
+        val readSideRequest: HandleReadSideRequest =
+          HandleReadSideRequest().withEvent(event).withState(resultingState).withMeta(meta).withReadSideId(processorId)
+
+        // send the request to the server
+        readSideRequestObserver.onNext(readSideRequest)
+      }
+    } catch {
+      case e: RuntimeException =>
+        logger.error(s"read side processing failure, processor=$processorId, cause=${e.getMessage}")
+        // Cancel RPC call
+        readSideRequestObserver.onError(e)
+        throw e;
+    }
+
+    // we tell the server that the client is done sending data
+    readSideRequestObserver.onCompleted()
+
+    // Receiving happens asynchronously.
+    doneSignal.await()
+  }
+}
