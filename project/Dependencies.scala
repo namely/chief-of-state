008a98871dfc73ce10cf4855d45658b6e099a022
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-03 23:41:32 +0200

    Update proto-google-common-protos-scalapb_0.10 to 2.5.0-2 (#407)
    Co-authored-by: Arsene Tochemey Gandote <tochemey@hotmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val AkkaVersion: String = "2.6.16"
     val SilencerVersion: String = "1.7.5"
     val LogbackVersion: String = "1.2.5"
-    val ScalapbCommonProtoVersion: String = "1.18.1-1"
+    val ScalapbCommonProtoVersion: String = "2.5.0-2"
     val ScalapbValidationVersion: String = "0.1.4"
     val ScalaTestVersion: String = "3.2.9"
     val AkkaManagementVersion: String = "1.1.1"
