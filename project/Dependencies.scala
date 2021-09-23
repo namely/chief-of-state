561407e8081679d873a2f113ee93727ce7a66604
Scala Steward <me@scala-steward.org>
2021-09-23 04:53:23 +0200

    Update otel-tools_2.13 to 0.1.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
 
     val TestContainers: String = "0.39.8"
 
-    val OtelToolsVersion: String = "0.1.7"
+    val OtelToolsVersion: String = "0.1.8"
   }
 
   import Dependencies.Versions._
