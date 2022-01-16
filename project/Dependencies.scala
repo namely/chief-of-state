00cc4f34b69297daea9eed7ea549d251c98eea25
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-16 18:31:02 +0100

    Update silencer-lib, silencer-plugin to 1.7.8 (#473)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   object Versions {
     val ScalaVersion: String = "2.13.6"
     val AkkaVersion: String = "2.6.17"
-    val SilencerVersion: String = "1.7.6"
+    val SilencerVersion: String = "1.7.8"
     val LogbackVersion: String = "1.2.6"
     val ScalapbCommonProtoVersion: String = "2.5.0-2"
     val ScalapbValidationVersion: String = "0.1.4"
