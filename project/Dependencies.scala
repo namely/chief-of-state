3370124edf8f4ed3f328232b3d576ce3edaef116
Scala Steward <scala_steward@virtuslab.com>
2022-09-07 18:22:27 +0000

    Update akka-actor-testkit-typed, ... to 2.6.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   // Package versions
   object Versions {
     val ScalaVersion: String = "2.13.6"
-    val AkkaVersion: String = "2.6.17"
+    val AkkaVersion: String = "2.6.20"
     val SilencerVersion: String = "1.7.8"
     val LogbackVersion: String = "1.2.10"
     val ScalapbCommonProtoVersion: String = "2.5.0-2"
