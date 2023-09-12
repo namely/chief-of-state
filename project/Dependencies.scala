0f2f4bb3b7d3b57a6f4382030584c3b71de07fc0
Scala Steward <scala_steward@virtuslab.com>
2023-09-12 21:54:37 +0000

    Update opentelemetry-sdk-testing to 1.30.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val JaninoVersion: String = "3.1.6"
     val LogstashLogbackVersion: String = "6.3"
 
-    val OpenTelemetryVersion: String = "1.11.0"
+    val OpenTelemetryVersion: String = "1.30.1"
 
     val TestContainers: String = "0.39.8"
 
